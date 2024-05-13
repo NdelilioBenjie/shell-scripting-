49c4cc2103a17c378f05cdee7041720bb14ae7f0
NdelilioBenjie <36301017+NdelilioBenjie@users.noreply.github.com>
2024-05-13 23:06:26 +0300

    Add files via upload
    scripts--added

---
+++ scripts/script2.sh
@@ -0,0 +1,8 @@
+#script2.sh
+echo "STARTING OF THE SHELL SCRIPT!!";
+echo "Current working directory is:";
+pwd
+echo "List of current files in the working directory:";
+ls
+echo "ENDING OF THE SHELL SCRIPT!!";
+cat script2.sh
