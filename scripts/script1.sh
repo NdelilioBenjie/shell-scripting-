91f23dcac10c9b0761f1a34cc3c8b0c74a55193a
NdelilioBenjie <36301017+NdelilioBenjie@users.noreply.github.com>
2024-05-13 23:18:05 +0300

    Delete scripts/script1.sh

--- scripts/script1.sh
+++
@@ -1,8 +0,0 @@
-#script1.sh
-echo "STARTING OF THE SHELL SCRIPT!!";
-echo "Output from running the commnand uname-a:";
-uname -a
-echo "Output from running the commnand uname-s:";
-uname -s
-echo "ENDING OF THE SHELL SCRIPT!!";
-cat script1.sh
