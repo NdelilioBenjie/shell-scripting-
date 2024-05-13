a3a8dda1bc0800c5aec9bdc8a2b83a3b04ff8370
NdelilioBenjie <36301017+NdelilioBenjie@users.noreply.github.com>
2024-05-13 23:18:43 +0300

    Delete scripts/script2.sh

--- scripts/script2.sh
+++
@@ -1,8 +0,0 @@
-#script2.sh
-echo "STARTING OF THE SHELL SCRIPT!!";
-echo "Current working directory is:";
-pwd
-echo "List of current files in the working directory:";
-ls
-echo "ENDING OF THE SHELL SCRIPT!!";
-cat script2.sh
